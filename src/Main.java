0c3c200ba01e1f18df06b8585d11fadeb0dd5406
DerekOde <derekodeste32@gmail.com>
2023-07-25 23:14:09 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a new vending machine
+        VendingMachine vendingMachine = new VendingMachine();
+        Interface vendingMachineInterface = new Interface();
+
+        // Start the interface
+        vendingMachineInterface.startMenu();
+    }
+}
